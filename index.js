88d123a76c005baba7c03166712068d61732f0b2
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-03 20:50:01 +0600

    Created json and chef details

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+const chefDetails = require("./data/chef.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/chef", (req, res) => {
+  res.send(chefDetails);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+
+  const selectedChef = chefDetails.find((n) => n.chef_id == id);
+  res.send(selectedChef);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
